f8a8a7797dfd625f0f7ea286a979e7f4a508bab8
Adrian F. Hernandez (afh23) <153098599+AdrianFHernandez@users.noreply.github.com>
2026-02-12 21:03:10 -0500

    Delete src directory

--- src/core/ICommand.ts
+++
@@ -1,5 +0,0 @@
-export interface ICommand {
-  name: string;
-  description: string;
-  execute(args: string[], opts?: any): Promise<void>;
-}
--- src/providers/ISearchProvider.ts
+++
@@ -1,4 +0,0 @@
-export interface ISearchProvider {
-  name: string;
-  search(query: string): Promise<any>;
-}
